37262308173c4a05b92a9b590d362d1e616ebb44
Diego <diego.sanches@gmail.com>
2019-10-16 18:41:55 +0000

    Initial commit

--- src/test/java/ar/edu/unq/eis/AppTest.java
+++ src/test/java/ar/edu/unq/eis/AppTest.java
@@ -1,38 +1,11 @@
 package ar.edu.unq.eis;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    @Test
+    void justAnExample() {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
     }
 }
